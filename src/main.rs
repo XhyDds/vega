eee95be7cc1fd0028a56befb29a897305f04be06
yuri <yzx_ustc@mail.ustc.edu.cn>
2023-06-13 17:13:10 +0800

    add pi calc for test

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,40 @@
-use vega::*;
+// use vega::*;
 
+// fn main() -> Result<()> {
+//     let sc = Context::new()?;
+//     let col = sc.make_rdd((0..10).collect::<Vec<_>>(), 32);
+//     //Fn! will make the closures serializable. It is necessary. use serde_closure version 0.1.3.
+//     let vec_iter = col.map(Fn!(|i| (0..i).collect::<Vec<_>>()));
+//     let res = vec_iter.collect().unwrap();
+//     println!("result: {:?}", res);
+//     Ok(())
+// }
+
+
+
+
+use rand::Rng;
+use vega::*;
+/*
+    蒙特卡洛算法计算pi
+ */
 fn main() -> Result<()> {
     let sc = Context::new()?;
-    let col = sc.make_rdd((0..10).collect::<Vec<_>>(), 32);
+    let sz=1000000;
+    let col = sc.make_rdd((0..sz).collect::<Vec<_>>(), 2);
     //Fn! will make the closures serializable. It is necessary. use serde_closure version 0.1.3.
-    let vec_iter = col.map(Fn!(|i| (0..i).collect::<Vec<_>>()));
-    let res = vec_iter.collect().unwrap();
-    println!("result: {:?}", res);
+    // let y: f64 = rng.gen();
+    let coordinate_iter = col.map(Fn!(|i|{
+        let mut rng = rand::thread_rng();
+        // let y:f64 = rng.gen();
+        let pair=(rng.gen_range(-100.0,100.0) as f64,rng.gen_range(-100.0,100.0) as f64);
+        if pair.0*pair.0+pair.1*pair.1<=100.0*100.0 {
+            1
+        }else{
+            0
+        }
+    }));
+    let res = coordinate_iter.fold(0, Fn!(|acc,i|acc+i)).unwrap();
+    println!("result: {:?}", 1.0*res as f64*4.0/sz as f64);
     Ok(())
 }
