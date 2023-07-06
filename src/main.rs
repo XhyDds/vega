68ea42244c4de55bfa41ded8426c261d88d334f9
hao <haoyu_t@mail.ustc.edu.cn>
2023-07-06 16:55:24 +0800

    test prometheus rust client

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,14 @@ mod benchmark;
 
 fn main() -> Result<()> {
     let sc: std::sync::Arc<Context> = Context::new()?;
+
+    tokio::spawn(monitor::metrics::add_metric());
+
     let start = Instant::now();
     benchmark::pi::calc_pi(&sc, Some(1000000), Some(2));
     // benchmark::e::calc_e(&sc, Some(10000), Some(3));
     let end = start.elapsed();
     println!("{:?}", end);
+
     Ok(())
 }
