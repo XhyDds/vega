f1214b88923c0b7484189f34fcec76298f38776c
4332001876 <mzfslhm@163.com>
2023-07-07 18:14:18 +0800

    multihead_attention

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ async fn main() -> Result<()> {
 
     let start = Instant::now();
     //benchmark::pi::calc_pi(&sc, Some(1000000), Some(3));
-    benchmark::multihead_attention::multihead_attention(&sc, Some(10000), Some(100), Some(3));
+    benchmark::multihead_attention::multihead_attention(&sc, Some(3));
     // benchmark::e::calc_e(&sc, Some(10000), Some(3));
     let end = start.elapsed();
     println!("{:?}", end);
