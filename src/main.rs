8118f700adb45a82340f2c952737bb1f4f12d6eb
yuri <yzx_ustc@mail.ustc.edu.cn>
2023-07-07 21:12:35 +0800

    yml change

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,8 @@ fn main() -> Result<()> {
 
     let start = Instant::now();
     //benchmark::pi::calc_pi(&sc, Some(1000000), Some(3));
-    //benchmark::wordcount::wordcount();
-    benchmark::multihead_attention::multihead_attention(&sc, Some(3));
+    benchmark::wordcount::wordcount();
+    // benchmark::multihead_attention::multihead_attention(&sc, Some(3));
     // benchmark::e::calc_e(&sc, Some(10000), Some(3));
     // benchmark::wordcount::wordcount(&sc,None);
     let end = start.elapsed();
