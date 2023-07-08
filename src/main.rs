c772cae2366f20de0f917ead35fe92bfe878cd46
yuri <yzx_ustc@mail.ustc.edu.cn>
2023-07-08 21:22:45 +0800

    change to e calc

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,18 @@ mod benchmark;
 async fn main() -> Result<()> {
     let sc: std::sync::Arc<Context> = Context::new().unwrap();
 
-    tokio::spawn(monitor::metrics::add_metric(sc.clone()));
+    // tokio::spawn(monitor::metrics::add_metric(sc.clone()));
 
     let start = Instant::now();
-    benchmark::pi::calc_pi(&sc, Some(1000000), Some(2));
+    // benchmark::pi::calc_pi(&sc, Some(1000000), Some(2));
     // benchmark::multihead_attention::multihead_attention(&sc, Some(10000), Some(100), Some(3));
-    // benchmark::e::calc_e(&sc, Some(10000), Some(3));
+    benchmark::e::calc_e(&sc, Some(10000), Some(3));
     // benchmark::wordcount::wordcount();
     let end = start.elapsed();
 
     println!("{:?}", end);
     
-    std::thread::sleep(std::time::Duration::from_secs(10));
+    //std::thread::sleep(std::time::Duration::from_secs(10));
 
     Ok(())
 }
