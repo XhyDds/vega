606ded66459c7898b40f569717ffdb35781a7dc3
4332001876 <mzfslhm@163.com>
2023-07-08 21:47:28 +0800

    new_pi

--- src/benchmark/pi.rs
+++ src/benchmark/pi.rs
@@ -10,25 +10,31 @@ use vega::*;
 #[allow(dead_code)]
 pub fn calc_pi(sc: &Arc<Context>, param: Option<i32>, num_slices: Option<usize>) {
     let start = Instant::now();
-    let param = param.unwrap_or(100000);
+    let param = param.unwrap_or(10);
+    //let batch_size:i32=1000000;
     let num_slices = num_slices.unwrap_or(2);
     let col = sc.make_rdd(0..param, num_slices);
 
     //Fn! will make the closures serializable. It is necessary. use serde_closure version 0.1.3.
     let coordinate_iter = col.map(Fn!(|_| {
         let mut rng = rand::thread_rng();
-        let pair = (
-            rng.gen_range(-100.0f64, 100.0f64) as f64,
-            rng.gen_range(-100.0f64, 100.0f64) as f64,
-        );
-        if pair.0 * pair.0 + pair.1 * pair.1 <= 100.0 * 100.0 {
-            1
-        } else {
-            0
+        let mut sum: f64 = 0;
+        for _ in 0..1000000 {
+            let pair = (
+                rng.gen_range(-100.0f64, 100.0f64) as f64,
+                rng.gen_range(-100.0f64, 100.0f64) as f64,
+            );
+            let tmp_res = if pair.0 * pair.0 + pair.1 * pair.1 <= 100.0 * 100.0 {
+                1.0f64
+            } else {
+                0.0f64
+            };
+            sum += tmp_res;
         }
+        sum * 4.0 / 1000000.0f64
     }));
     let res = coordinate_iter.fold(0, Fn!(|acc, i| acc + i)).unwrap();
-    println!("result: {:?}", (res as f64) * 4.0 / (param as f64));
+    println!("result: {:?}", (res as f64) / (param as f64));
 
     let end = start.elapsed();
     println!("Cal_pi_time_cost: {:?}", end);
