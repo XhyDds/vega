cafc1ef09b82b643965eef90797cd7b967a9abf8
hao <haoyu_t@mail.ustc.edu.cn>
2023-07-08 16:16:13 +0800

    update docker

--- examples/make_rdd.rs
+++ examples/make_rdd.rs
@@ -1,11 +1,17 @@
 use vega::*;
 
-fn main() -> Result<()> {
+#[tokio::main]
+async fn main() -> Result<()> {
     let sc = Context::new()?;
+
+    tokio::spawn(monitor::metrics::add_metric(sc.clone()));
+
     let col = sc.make_rdd((0..10).collect::<Vec<_>>(), 32);
     //Fn! will make the closures serializable. It is necessary. use serde_closure version 0.1.3.
     let vec_iter = col.map(Fn!(|i| (0..i).collect::<Vec<_>>()));
     let res = vec_iter.collect().unwrap();
     println!("result: {:?}", res);
+
+    std::thread::sleep(std::time::Duration::from_secs(10));
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -9,15 +9,14 @@ async fn main() -> Result<()> {
     tokio::spawn(monitor::metrics::add_metric(sc.clone()));
 
     let start = Instant::now();
-    // benchmark::pi::calc_pi(&sc, Some(1000000), Some(2));
+    benchmark::pi::calc_pi(&sc, Some(1000000), Some(2));
     // benchmark::multihead_attention::multihead_attention(&sc, Some(10000), Some(100), Some(3));
     // benchmark::e::calc_e(&sc, Some(10000), Some(3));
-    benchmark::wordcount::wordcount();
+    // benchmark::wordcount::wordcount();
     let end = start.elapsed();
 
     println!("{:?}", end);
-
-    // sleep for 5 seconds
+    
     std::thread::sleep(std::time::Duration::from_secs(10));
 
     Ok(())
